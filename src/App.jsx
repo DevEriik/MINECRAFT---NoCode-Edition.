1521a3ec6cc140a01c2b7f931dea35bdcfd54346
Abril Gavilan <abrilcgavilan@gmail.com>
2026-04-23 23:28:37 -0300

    Realizacion de Componentes Header, Footer y Multi Idioma. Además, hice modificaciones en archivos como App.jsx para el renderizado, main.jsx para la importación de multi idioma, agregué imagenes para el logo (está para mejorarse) y avatares.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import "./i18n";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <BrowserRouter>
-      {/* <Header /> */}
-      <main>
-        <Routes>
-          <Route
-            path="/"
-            element={<div>Componente en construccion</div>}
-          ></Route>
-        </Routes>
-      </main>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-grow p-4 lg:p-8">
+          <Routes>
+            <Route path="/" element={<div>Componente en construcción</div>} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
